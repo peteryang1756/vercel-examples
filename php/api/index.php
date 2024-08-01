867e30d2b577dd65428b54bfe9a5cb2b53e86581
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-08-01 13:25:17 +0800

    Update index.php

--- php/api/index.php
+++ php/api/index.php
@@ -1,4 +1,49 @@
 <?php
+$config = array(
+    'url' => 'https://help.ssangyongsports.eu.org/help/api/tickets.json',
+    'key' => '7A708D9E06A3D085C2BA16A48513F4F1'
+);
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = array(
+        'name' => $_POST['name'],
+        'email' => $_POST['email'],
+        'subject' => $_POST['subject'],
+        'message' => $_POST['message'],
+        'ip' => $_SERVER['REMOTE_ADDR'],
+        'attachments' => array(),
+    );
+
+    function_exists('curl_version') or die('CURL support required');
+    function_exists('json_encode') or die('JSON support required');
+
+    set_time_limit(30);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $config['url']);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_USERAGENT, 'osTicket API Client v1.7');
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:', 'X-API-Key: ' . $config['key']));
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $result = curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if (curl_errno($ch)) {
+        die('cURL error: ' . curl_error($ch));
+    }
+
+    curl_close($ch);
+
+    if ($code != 201) {
+        die('Unable to create ticket: ' . $result);
+    }
+
+    $ticket_id = json_decode($result, true)['ticket_id'];
+    header("Location: /thanks?id=" . urlencode($ticket_id));
+    exit();
+}
+?>
